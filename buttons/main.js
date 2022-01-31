b33ca203f9cfb56f2b00956ab7170ae9d8bd2c08
Hilary Fung <hilary.w.fung@gmail.com>
2022-01-31 01:28:41 -0800

    create links page

--- buttons/main.js
+++ buttons/main.js
@@ -4,7 +4,7 @@ const photo = document.querySelector("#photo")
 
 function change_animal(animal) {
   text.innerHTML = "Here's a picture of a " + animal + "."
-  photo.src = animal + ".jpg"
+  photo.src = "../images/" + animal + ".jpg"
 }
 
 animal_buttons.forEach(button => {
