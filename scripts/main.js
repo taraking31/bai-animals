b12d148674c795643178dc0213ddc91aa36ff869
Hilary Fung <hilary.w.fung@gmail.com>
2022-01-30 22:46:08 -0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+const animal_buttons = document.getElementsByName("animal-button")
+const text = document.querySelector("#text")
+const photo = document.querySelector("#photo")
+
+function change_animal(animal) {
+  text.innerHTML = "Here's a picture of a " + animal + "."
+  photo.src = animal + ".jpg"
+}
+
+animal_buttons.forEach(button => {
+  button.addEventListener("change", event => {
+    change_animal(button.value)
+  })
+})
